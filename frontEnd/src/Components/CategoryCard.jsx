81ce4943393238f7b6f8c0793c40d86b502419ae
DESKTOP-R5P3GEM\CodeAcademy <tatjana.sibik@gmail.com>
2024-06-26 21:03:47 +0300

    Categories cards created

--- frontEnd/src/Components/CategoryCard.jsx
+++ frontEnd/src/Components/CategoryCard.jsx
@@ -1,8 +1,8 @@
-import styles from "./CategoryList.module.css";
 import PropTypes from 'prop-types';
+import styles from "./CategoryList.module.css";
 
 const CategoryCard = ({ category }) => {
-  const {name, img} = category;
+  const { name, img } = category;
 
   return (
     <div className={styles.card}>
@@ -13,10 +13,10 @@ const CategoryCard = ({ category }) => {
 };
 
 CategoryCard.propTypes = {
-    category: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      img: PropTypes.string,
-    }).isRequired,
-  };
+  category: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    img: PropTypes.string,
+  }).isRequired,
+};
 
 export default CategoryCard;
